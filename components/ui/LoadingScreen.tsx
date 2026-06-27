06ce068c9194365095fa0834113930fc7003af60
Manohar Tej <manohartejkandula@gmail.com>
2026-06-27 16:04:13 +0530

    Fix animejs overriding CSS width by scaling parent container

--- components/ui/LoadingScreen.tsx
+++ components/ui/LoadingScreen.tsx
@@ -197,7 +197,7 @@ export default function LoadingScreen() {
         ))}
       </div>
 
-      <div className="mobile-landscape-scale" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '30px', zIndex: 10 }}>
+      <div className="mobile-landscape-scale mobile-portrait-scale" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '30px', zIndex: 10 }}>
         
         {/* Unrolling Film Reel MEMORY */}
         <div className="film-reel-container" style={{
